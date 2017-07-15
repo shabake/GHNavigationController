7719a9e11f1d13045c316d81edbb8e20b5356e2f
TwinkleOne <mfj9105@icloud.com>
2017-07-15 09:28:15 +0800

    Initial Commit

---
+++ 测试导航条左侧item响应/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  测试导航条左侧item响应
+//
+//  Created by eiboran-imac5k on 2017/7/15.
+//  Copyright © 2017年 eiboran-imac5k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 测试导航条左侧item响应/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  测试导航条左侧item响应
+//
+//  Created by eiboran-imac5k on 2017/7/15.
+//  Copyright © 2017年 eiboran-imac5k. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
